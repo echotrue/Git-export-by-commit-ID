915be100a6928454e50a612ef66089930c275ef6
axlrose <axlrose.huang@gmail.com>
2020-05-22 18:01:54 +0800

    format code && pretty

--- export.sh
+++ export.sh
@@ -6,34 +6,32 @@
 # Description: Script for git export
 # -------------------------------------------------
 
-#file dir
+
+# Project directory
 file_dir=/xxx/xxx/xxx/
-#export dir
+# Export directory
 export_dir=/xxx/xxx/xxx/
 
 for key2 in $*
 do
+    echo -e "\033[33mcommit ${key2}\033[0m"
     file_list=$(git show --name-only --pretty=format:%n $key2)
+
     for file in $file_list
     do
-      echo -e "\033[37mExport file:$file \033[0m"
+      echo -e "--${file}"
       file_path=$file_dir$file
       export_path=$export_dir$file
 
       export_path_dir=$(dirname $export_path)
       if [ ! -d $export_path_dir ]; then
-          # Get dir
           mkdir -p -m 755 $export_path_dir
-          echo -e "\033[32mCreate dir: $export_path_dir done \033[0m"
       fi
 
-      #copy
-      cp_re=$(cp -rvf $file_path $export_path)
-#      echo $cp_re
-      echo -e "\033[32mDone！ \033[0m"
+      $(cp -rvf $file_path $export_path)
     done
-done
 
-echo $'\n'
+    echo -e "\033[32mcommit ${key2} Export complete！ \033[0m"
+done
 
 read -p "Press any key to close the window！" -n 1
